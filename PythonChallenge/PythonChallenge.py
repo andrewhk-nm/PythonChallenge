2ebf36f28f6d50b0f2db09e8e58ab59b528e6a47
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2017-12-28 14:32:02 -0500

    Attempting #3

--- PythonChallenge/PythonChallenge.py
+++ PythonChallenge/PythonChallenge.py
@@ -2,13 +2,15 @@
 http://www.pythonchallenge.com/
 """
 
-def problem1():
+import os
+
+def problem0():
     """ http://www.pythonchallenge.com/pc/def/0.html
     Answer = 274877906944
     """
     print(2**38)
 
-def problem2():
+def problem1():
     """ http://www.pythonchallenge.com/pc/def/map.html
     everybody thinks twice before solving this.
 
@@ -31,13 +33,27 @@ def problem2():
     print('http://www.pythonchallenge.com/pc/def/map.html'.translate(k_to_m_dict))
 
 
-def problem3():
+def problem2():
     """ http://www.pythonchallenge.com/pc/def/ocr.html
     recognize the characters. maybe they are in the book, 
     but MAYBE they are in the page source.
     """
 
+    # Create an empty dictionary to store the counts of characters
+    unique_char_counter = dict()
+    with open('problem2_mess_below.txt', encoding='utf-8') as p2_file:
+        # I want to read the file character by character but I can't really figure out how.
+        for p2_line in p2_file.read(1):
+            # I can't figure out how to iterate through the stringgnngngngngngngn 
+            for char_key in p2_line:
+                try:
+                    # If there is already a key counting the characters, add to it.
+                    unique_char_counter[char_key] += unique_char_counter.get(char_key)
+                except KeyError:
+                    # Key does not yet exist, add it with an inital count of 1
+                    unique_char_counter[char_key] = 1
 
+    print(unique_char_counter)
 
 
 if __name__ == '__main__':
