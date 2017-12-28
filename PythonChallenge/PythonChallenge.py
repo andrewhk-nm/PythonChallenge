55abdfc4ebfce9db13aa628e869bb2a76c9b7ec1
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2017-12-28 13:42:37 -0500

    Add project files.

---
+++ PythonChallenge/PythonChallenge.py
@@ -0,0 +1,12 @@
+""" This is my collection of attempts at the Python Challenge
+http://www.pythonchallenge.com/
+"""
+
+def problem1():
+    """ http://www.pythonchallenge.com/pc/def/0.html
+    Answer = 274877906944
+    """
+    print(2**38)
+
+if __name__ == '__main__':
+    problem1()
