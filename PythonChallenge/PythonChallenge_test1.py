dda642709ed1447a059f338afd88bf6330c1bfe4
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-01-31 13:00:06 -0500

    updating PythonChallenge to work with external module helperfunctions

--- PythonChallenge/PythonChallenge_test1.py
+++ PythonChallenge/PythonChallenge_test1.py
@@ -1,21 +1,7 @@
 import unittest
 from PythonChallenge import HelperFunctions
 
-class Test_PythonChallenge_test1(unittest.TestCase):
-    known_answers_gen_char_neighbor = [('U', 'kAeMKENi'),
-                                       ('K', 'AewULNih'),
-                                       ('L', 'ewtKEihr')
-                                       ]
-    
-    def test_HelperFunctions_gen_char_neighbor_known_answers(self):
-        """ Test some known answers of the generator.
-        """
-        test_gen = HelperFunctions.gen_char_neighbor('problem3_source_mess.txt')
-        for center, neighbors in self.known_answers_gen_char_neighbor:
-            print('center={}, neighbors={}'.format(center, neighbors))
-            test_ans = next(test_gen)
-            print('test_ans={}'.format(test_ans))
-            self.assertEqual((center, neighbors), test_ans)
+
     
     
 
