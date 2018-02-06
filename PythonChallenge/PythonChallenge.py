265d6b72ebee7fa93b6e5dce9704690a0d6c7ab8
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-02-06 12:44:13 -0500

    Revert "externalized the helperfunctions module"
    This reverts commit d673f3fd09ee23d96849ecc49bc0f82a95b58fb5.

--- PythonChallenge/PythonChallenge.py
+++ PythonChallenge/PythonChallenge.py
@@ -8,7 +8,99 @@ import os
 import string
 from collections import namedtuple
 
-import helperfunctions as hf
+class HelperFunctions:
+    """ Store my helper functions here for easier testing and re-use.
+    """
+    def lookup_bodyguards(self, grid, center, guards=3):
+        """ takes a list of text strings representing a 'grid'. 
+        Given a 'center' (row, col), returns 'guards' number of adjacent letters.
+
+               
+       e.g. guards=3, center=(3, 3)
+            A
+            B
+            C
+         JKLxDEF
+            G
+            H
+            I
+
+        returns [A, B, C, D, E, F, G, H, I, J, K, L]
+        """
+
+
+    def process_text(self, filename):
+        """ Takes a filename that holds a rectangular block of text.
+        Return a list strings from that block.
+        
+
+
+
+        """
+        # Originally for problem 3
+        
+        # Create a list of lists. Each member list is a line of text.
+        a_list = list()
+        with open(filename, encoding='utf-8') as a_file:
+            for a_line in a_file:
+                a_list.append(a_line.strip())          
+
+        return a_list
+        
+
+    def get_char_count(filename):
+        """ Wrapper for the problem2 solution.
+        Takes the filename of a text file, and returns a dictionary. 
+        The keys are the character and the value is the character count.
+        """
+        return problem2(filename, print_debug_info=True)
+
+    def gen_char_neighbor(filename):
+        """ take a filename of a text file in block format.
+        generates the neighbors for the next char.
+        returns tuple: (char, '12346789') where:
+        123
+        456
+        789
+        """
+        a_list = list()
+        with open(filename, encoding='utf-8') as a_file:
+            for a_line in a_file:
+                a_list.append(a_line.rstrip())
+
+        #print('a_list={}'.format(a_list))
+
+        # Start at line 2 char 2, end at the 2nd to last line, 2nd to last char
+        for line in range(1, len(a_list) - 1):
+            for char in range(1, len(a_list[line]) - 1):
+                #print('a_list[{}][{}] = {}'.format(line, char, a_list[line][char]))
+                # So for each letter in the block, check each surrounding letter, and count the Big letters.
+                #     (x-1)-row_length  x-row_length  (x+1)-row_length 
+                #     (x-1)             x             (x+1)
+                #     (x-1)+row_length  x+row_length  (x+1)+row_length 
+                # If Count(outsides) = 3 big letters then success
+                Line_char = namedtuple('Line_char', 'line char')
+                line_char_1 = Line_char(line - 1, char - 1)
+                line_char_2 = Line_char(line - 1, char)
+                line_char_3 = Line_char(line - 1, char + 1)
+                line_char_4 = Line_char(line, char - 1)
+                line_char_6 = Line_char(line, char + 1)
+                line_char_7 = Line_char(line + 1, char - 1)
+                line_char_8 = Line_char(line + 1, char)
+                line_char_9 = Line_char(line + 1, char + 1)
+                line_char_all = ''.join([a_list[line_char_1.line][line_char_1.char],
+                                         a_list[line_char_2.line][line_char_2.char],
+                                         a_list[line_char_3.line][line_char_3.char],
+                                         a_list[line_char_4.line][line_char_4.char],
+                                         a_list[line_char_6.line][line_char_6.char],
+                                         a_list[line_char_7.line][line_char_7.char],
+                                         a_list[line_char_8.line][line_char_8.char],
+                                         a_list[line_char_9.line][line_char_9.char],
+                                        ])
+                                
+                yield (a_list[line][char], line_char_all)
+
+
 
 def problem0():
     """ http://www.pythonchallenge.com/pc/def/0.html
@@ -152,7 +244,9 @@ def problem3_mark2():
     #    A
     #    A
 
-    gen_bodyguards = hf.process_text('problem3_source_mess.txt')
+    Hf = HelperFunctions()
+
+    gen_bodyguards = Hf.process_text('problem3_source_mess.txt')
     for x in gen_bodyguards:
         print(x)
 
